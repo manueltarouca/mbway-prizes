cbc8d881cdb843cc9da7d5039cd4e9d8bece4080
mt <manuel+git@matm.xyz>
2022-08-30 19:17:20 +0100

    Fix database path

--- backend/api/mbway.ts
+++ backend/api/mbway.ts
@@ -3,8 +3,7 @@ import moment from 'moment';
 import fs from 'fs'
 
 export default async function handler(req, res) {
-  console.log(fs.readdirSync(process.cwd()));
-  const db = new Database('./db/mbway-prizes.sqlite', { readonly: true });
+  const db = new Database('./backend/db/mbway-prizes.sqlite', { readonly: true });
   const currentDates = [moment(new Date()).format('D/M/yyyy'), moment(new Date()).format('D/MM/yyyy')];
   const rows = db.prepare('SELECT * FROM prizes WHERE date IN (?, ?)').all(currentDates);
   return res.status(200).json({ items: rows });
