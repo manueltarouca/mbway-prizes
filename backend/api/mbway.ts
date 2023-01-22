b4542ed5270c011a3ef805540cd4eefe741d7a4d
mt <manuel+git@matm.xyz>
2023-01-22 23:01:16 +0000

    updated to year 2023

--- backend/api/mbway.ts
+++ backend/api/mbway.ts
@@ -1,11 +1,10 @@
 import Database from 'better-sqlite3';
 import moment from 'moment';
-import fs from 'fs'
 
 export default async function handler(req, res) {
   const db = new Database('./backend/db/mbway-prizes.sqlite', { readonly: true });
-  const currentDates = [moment(new Date()).format('D/M/yyyy'), moment(new Date()).format('D/MM/yyyy')];
-  const tomorowDates = [moment(new Date()).add(1,'day').format('D/M/yyyy'), moment(new Date()).add(1,'day').format('D/MM/yyyy')];
+  const currentDates = [moment(new Date()).format('dd/mm/yyyy'), moment(new Date()).format('dd/mm/yyyy')];
+  const tomorowDates = [moment(new Date()).add(1,'day').format('dd/mm/yyyy'), moment(new Date()).add(1,'day').format('dd/mm/yyyy')];
   const todayRows = db.prepare('SELECT * FROM prizes WHERE date IN (?, ?)').all(currentDates);
   const tomorowRows = db.prepare('SELECT * FROM prizes WHERE date IN (?, ?)').all(tomorowDates);
   return res.status(200).json({ today: todayRows, tomorow: tomorowRows });
